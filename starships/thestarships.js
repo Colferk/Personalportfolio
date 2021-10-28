4d12ec3df0f0c399c8d0c9006a20515e999f9f0a
Colferk <countcheeseula@gmail.com>
2021-10-28 10:57:58 -0600

    name

---
+++ starships/thestarships.js
@@ -0,0 +1,9 @@
+import { starships } from "../Data/starships";
+
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".navList");
+const shipView = document.querySelector(".desplaySection");
+
+function populateView(starships) {
+   
+}
