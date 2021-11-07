93e99c83a16f70af7172e0eb43af132c09908278
Colferk <countcheeseula@gmail.com>
2021-11-06 18:18:36 -0600

    bracket

--- Film/films.js
+++ Film/films.js
@@ -1,3 +1,17 @@
 import { films } from '../Data/films';
 
 import { getLastNumber } from '../index'
+
+let filmList = document.querySelector('#filmList')
+
+for (let i = 0; i < films.length; i++) {
+
+    let figure = document.createElement('figure')
+
+    let figImg = document.createElement('img')
+
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+
+    let figCaption = document.createElement('figcaption')
+
+}
