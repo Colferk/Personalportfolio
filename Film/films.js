52e9ceee102f112d24eb4a48b31ebf219e6eac00
Colferk <countcheeseula@gmail.com>
2021-11-08 10:53:30 -0700

    side text

--- Film/films.js
+++ Film/films.js
@@ -1,36 +1,24 @@
-import { films } from '../Data/films';
+import { films } from "../data/films.js";
+import { getLastNumber } from "../utils/index.js";
 
-import { getLastNumber } from '../index'
-
-let filmList = document.querySelector('#filmList')
+let filmList = document.querySelector("#filmList");
 
 for (let i = 0; i < films.length; i++) {
+  let figure = document.createElement("figure");
+  let figImg = document.createElement("img");
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`;
+  let figCaption = document.createElement("figcaption");
 
-    let figure = document.createElement('figure')
+  const foundFilm = films.find((film) => {
+    const convertedString = parseInt(getLastNumber(film.url), 10);
+    return convertedString === i + 1;
+  });
 
-    let figImg = document.createElement('img')
+  console.log(foundFilm);
 
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+  figCaption.textContent = foundFilm.title;
 
-    let figCaption = document.createElement('figcaption')
-
+  figure.appendChild(figImg);
+  figure.appendChild(figCaption);
+  filmList.appendChild(figure);
 }
-
-
-const foundFilm = films.find(film => {
-
-    const convertedString = parseInt(getLastNumber(film.url), 10)
-
-    return convertedString === (i + 1)
- })
-
- figCaption.textContent = foundFilm.title
-  
- figure.appendChild(figImg)
-
- figure.appendChild(figCaption)
-
- filmList.appendChild(figure)
-
-
- console.log(foundFilm)
