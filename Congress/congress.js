f4183c0d63949a9d345ba0417b7ad1d4b5041db4
Colferk <countcheeseula@gmail.com>
2021-11-17 19:00:38 -0700

    format

--- Congress/congress.js
+++ Congress/congress.js
@@ -1,38 +1,48 @@
-import { senators } from '../Data/senators'
-import { representatives} from '../Data/representatives'
+import { senators } from "../Data/senators";
+import { representatives } from "../Data/representatives";
 
-const members = [...senators, ...representatives]
+const members = [...senators, ...representatives];
 
-console. log (members.length)
+console.log(members.length);
 
-const senatorDiv = document.querySelector('.senators')
+const senatorDiv = document.querySelector(".senators");
 
-const loyaltyHeading = document.querySelector('.mostLoyal')
+const loyaltyHeading = document.querySelector(".mostLoyal");
 
-const seniorityHeading = document.querySelector('.seniority')
-
+const seniorityHeading = document.querySelector(".seniority");
 
 function SimplifiedMembers(chamberFilter) {
+  const filteredArray = members.filter((member) =>
+    chamberFilter ? member.short_title === chamberFilter : member
+  );
 
-    const filteredArray = members.filter((member) =>
+  return filteredArray.map((senator) => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
 
-      chamberFilter ? member.short_title === chamberFilter : member,
-    )
+    return {
+      id: senator.id,
+      name: `${senator.first_name}${middleName}${senator.last_name}`,
+      party: senator.party,
+      gender: senator.gender,
+      seniority: +senator.seniority,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+      missedVotesPct: senator.missed_votes_pct,
+      loyaltyPct: senator.votes_with_party_pct,
+    };
+  });
+}
 
-return filteredArray.map((senator) => {
+function populateSenatorDiv(simpleSenators) {
+  simpleSenators.forEach((senator) => {
+    const senFigure = document.createElement("figure");
+    const figImg = document.createElement("img");
+    const figCaption = document.createElement("figcaption");
 
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    figImg.src = senator.imgURL;
+    figCaption.textContent = senator.name;
 
-        return {
-
-          id: senator.id,
-          name: `${senator.first_name}${middleName}${senator.last_name}`,
-          party: senator.party,
-          gender: senator.gender,
-          seniority: +senator.seniority,
-          imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
-          missedVotesPct: senator.missed_votes_pct,
-          loyaltyPct: senator.votes_with_party_pct,
-        }
-      })
-    }
+    senFigure.appendChild(figImg);
+    senFigure.appendChild(figCaption);
+    senatorDiv.appendChild(senFigure);
+  });
+}
