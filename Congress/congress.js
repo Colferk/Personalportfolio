c6011524d9644c910b5eec25ec6ada3136149b13
Colferk <countcheeseula@gmail.com>
2021-11-17 18:59:12 -0700

    const add

--- Congress/congress.js
+++ Congress/congress.js
@@ -7,3 +7,32 @@ console. log (members.length)
 
 const senatorDiv = document.querySelector('.senators')
 
+const loyaltyHeading = document.querySelector('.mostLoyal')
+
+const seniorityHeading = document.querySelector('.seniority')
+
+
+function SimplifiedMembers(chamberFilter) {
+
+    const filteredArray = members.filter((member) =>
+
+      chamberFilter ? member.short_title === chamberFilter : member,
+    )
+
+return filteredArray.map((senator) => {
+
+        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+
+        return {
+
+          id: senator.id,
+          name: `${senator.first_name}${middleName}${senator.last_name}`,
+          party: senator.party,
+          gender: senator.gender,
+          seniority: +senator.seniority,
+          imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+          missedVotesPct: senator.missed_votes_pct,
+          loyaltyPct: senator.votes_with_party_pct,
+        }
+      })
+    }
